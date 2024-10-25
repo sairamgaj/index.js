1b2bd451b0cc5fe59ceb010bb0477f7d3b384116
sairamgaj <sairamgajabhinkar19@gmail.com>
2024-10-25 18:55:05 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,36 @@
+console.log(x); 
+var x=5; //undefined
+
+console.log(a);
+var a; //undefined
+
+console.log(b);
+b=10;
+var b;
+
+//console.log(c); //it throws error
+
+console.log(e);
+var e=10;
+console.log(e);
+e=20;
+console.log(e);
+
+console.log(f);
+var f=100;
+var f;
+console.log(f);
+
+console.log(g)
+var g=g+1;
+console.log(g);
+
+var h;
+console.log(h);
+h=50;
+console.log(h);
+
+console.log(i);
+i=10;
+var i=5;
+console.log(i);
