67c70dc76e05e96277ce829765c64fb2d595040d
sairamgaj <sairamgajabhinkar19@gmail.com>
2024-10-25 18:45:08 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,17 @@
+console.log(x); 
+var x=5; //undefined
+
+console.log(a); //reference error
+var a; //undefined
+
+b = 10; //undefined
+console.log(b); //reference error b is not defined
+
+
+console.log(e); //undefined
+
+var e=10;
+console.log(e); //10
+
+e=20;
+console.log(e);//20
