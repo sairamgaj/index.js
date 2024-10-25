5879043f30a227957affe3eee78702aef89b9501
sairamgaj <sairamgajabhinkar19@gmail.com>
2024-10-25 18:39:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hello World!");// to see it in console
+ var b;
+b=25;
+console.log(b)
+
+var c=40;
+console.log(c)
+
+var d;  //undefined
+console.log(d)
+
+var e; //NaN
+console.log(e+35)
+
+var f;
+var g;
+console.log(f+g);
+
+var h="Hello";
+var i="Welcome to Hyderabad";
+console.log(h+i);
+console.log(h,i);
