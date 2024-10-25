38bdd46577a7ba51a0f9c8616dd0be67bef1c69d
sairamgaj <sairamgajabhinkar19@gmail.com>
2024-10-25 19:31:53 +0530

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,55 @@
+var trafficlights="red";
+if(trafficlights=="red"){
+    console.log("Stop the vehicle")
+    
+}
+else if (trafficlights=="yellow"){
+    console.log("Get ready and move the vehicle")
+                                                              //output : stop the vehicle
+}
+else if(trafficlights="green"){
+    console.log("Go")
+}
+else{
+    console.log("Invalid input")
+}
+
+var temparature="15";
+if(temparature=="0"){
+    console.log("It's freezing cold")
+}
+else if(temparature=="15"){
+    console.log("It's a cool day")
+}
+else if(temparature=="25"){
+    console.log("It's a pleasant day")                           //output : It's a cool day
+}
+else {
+    console.log("Unknown weather")
+}
+
+var num="8";
+if(num=="1"){
+    console.log("Today is Monday")
+}
+else if(num=="2"){
+    console.log("Today is Tuesday")
+}                                                              //output : Invaild day number
+else if(num=="3"){
+    console.log("Today is Wednesday")
+}
+else if(num=="4"){
+    console.log("Today is Thursday")
+}
+else if(num=="5"){
+    console.log("Today is Thursday")
+}
+else if(num=="6"){
+    console.log("Today is Friday")
+}
+else if(num=="7"){
+    console.log("Today is Saturday")
+}
+else{
+    console.log("Invalid day number")
+}
